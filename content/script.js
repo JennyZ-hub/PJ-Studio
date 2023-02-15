7e92419d52886ea933e5537c7db2e9ea72814d94
Jenny Zeng <420337484@qq.com>
2023-02-15 13:58:02 +1100

    add nav to other parts

--- content/script.js
+++ content/script.js
@@ -1,7 +1,23 @@
 'use strict';
 
 const navUpcomingEvent = document.querySelector('.nav--upcoming-events');
+const navTestimonials = document.querySelector('.nav--testimonials');
+const navPricing = document.querySelector('.nav--pricing');
+const navBook = document.querySelector('.nav--book');
 const sectionEvents = document.querySelector('.section-events');
+const sectionTestimonials = document.querySelector('.section-testimonials');
+const sectionPrice = document.querySelector('.section-price');
+const sectionBooking = document.querySelector('.section-form');
 navUpcomingEvent.addEventListener('click', function (e) {
   sectionEvents.scrollIntoView({ behavior: 'smooth' });
+});
+navTestimonials.addEventListener('click', function (e) {
+  console.log(sectionTestimonials);
+  sectionTestimonials.scrollIntoView({ behavior: 'smooth' });
+});
+navPricing.addEventListener('click', function (e) {
+  sectionPrice.scrollIntoView({ behavior: 'smooth' });
+});
+navBook.addEventListener('click', function (e) {
+  sectionBooking.scrollIntoView({ behavior: 'smooth' });
 });
